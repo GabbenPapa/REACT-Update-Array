f08f8325905911d7c7e188972664c59219220cfe
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2024-06-15 22:18:53 +0200

    Add Update Array

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import MyComponent from './MyComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    MyComponent()
+  )
+}
+
+export default App
